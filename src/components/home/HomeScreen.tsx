cfc2d7dc5ef132b5f1b146cc0e4f1fddc484586e
cashupin <jalvarezc96@gmail.com>
2022-06-30 00:44:05 -0400

    Basic design

--- src/components/home/HomeScreen.tsx
+++ src/components/home/HomeScreen.tsx
@@ -3,12 +3,25 @@ import React from 'react'
 
 export const HomeScreen: React.FC<RouteComponentProps> = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 text-center w-full">
-      <div className="w-full p-10 bg-red-50">
-        <p className='text-[40px] font-bold'>Juan Alvarez</p>
-        <p>Desarrollador</p>
+    <div className="grid grid-cols-1 md:grid-cols-2 text-center w-full h-full content-center bg-gray-800">
+      <div className="w-full grid grid-rows-3 p-20 text-white">
+        <div className="text-[4em] font-bold text-shadow-white leading-[0.90] ">
+          Juan Alvarez Carrera
+        </div>
+        <div className="">Developer</div>
+        <div>
+          <button className="border-solid border-2 border-white px-4 py-3 rounded-xl hover:bg-gray-700 active:bg-gray-600 m-3">
+            Download Cv
+          </button>
+          <button className="border-solid border-2 border-green-300 px-4 py-3 rounded-xl hover:bg-green-700 active:bg-green-600">
+            <i className="fab fa-whatsapp mr-2 text-green-300"></i>
+            Contactar
+          </button>
+        </div>
       </div>
-      <div>2</div>
+      <div className="">
+        <p>Imagen</p>
+      </div>
     </div>
   )
 }
--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -5,15 +5,9 @@ export const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false)
   return (
     <>
-      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-slate-500 mb-3">
+      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-gray-800 ">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <Link
-              className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
-              to="/"
-            >
-              Juan Alvarez
-            </Link>
             <button
               className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
               type="button"
@@ -32,26 +26,26 @@ export const Navbar = () => {
             <ul className="flex flex-col lg:flex-row list-none mr-auto text-right">
               <li className="nav-item">
                 <Link
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  className="px-3 py-2 flex items-center text-xl uppercase font-bold leading-snug text-white hover:opacity-75"
                   to="/"
                 >
-                  <span className="ml-2">Home</span>
+                  <span className="ml-2 text-shadow-white">Home</span>
                 </Link>
               </li>
               <li className="nav-item">
                 <Link
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  className="px-3 py-2 flex items-center text-xl uppercase font-bold leading-snug text-white hover:opacity-75"
                   to="stack"
                 >
-                  <span className="ml-2">Stack</span>
+                  <span className="ml-2 text-shadow-white">Stack</span>
                 </Link>
               </li>
               <li className="nav-item">
                 <Link
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  className="px-3 py-2 flex items-center text-xl uppercase font-bold leading-snug text-white hover:opacity-75"
                   to="projects"
                 >
-                  <span className="ml-2">Projects</span>
+                  <span className="ml-2 text-shadow-white">Projects</span>
                 </Link>
               </li>
             </ul>
@@ -59,42 +53,42 @@ export const Navbar = () => {
               <ul className="flex flex-col lg:flex-row list-none ml-auto">
                 <li className="nav-item">
                   <a
-                    className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                    className="px-3 py-2 flex items-center text-sm uppercase font-bold leading-snug text-white hover:opacity-75 rounded-xl text-shadow-white"
                     href="https://www.linkedin.com/in/jalvarezcarrera/"
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <i className="fab fa-linkedin text-lg leading-lg text-white opacity-75" />
+                    <i className="fab fa-linkedin text-3xl leading-lg text-white opacity-75" />
                   </a>
                 </li>
                 <li className="nav-item">
                   <a
-                    className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                    className="px-3 py-2 flex items-center text-sm uppercase font-bold leading-snug text-white hover:opacity-75 text-shadow-white"
                     href="https://twitter.com/jalvarezc96"
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <i className="fab fa-twitter text-lg leading-lg text-white opacity-75" />
+                    <i className="fab fa-twitter text-3xl leading-lg text-white opacity-75" />
                   </a>
                 </li>
                 <li className="nav-item">
                   <a
-                    className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                    className="px-3 py-2 flex items-center text-sm uppercase font-bold leading-snug text-white hover:opacity-75 text-shadow-white"
                     href="https://api.whatsapp.com/send?phone=56934305335&text=Hola%20Juan%2C%20te%20contacto%20a%20traves%20de%20tu%20portafolio."
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <i className="fab fa-whatsapp text-lg leading-lg text-white opacity-75" />
+                    <i className="fab fa-whatsapp text-3xl leading-lg text-white opacity-75" />
                   </a>
                 </li>
                 <li className="nav-item">
                   <a
-                    className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                    className="px-3 py-2 flex items-center text-sm uppercase font-bold leading-snug text-white hover:opacity-75 text-shadow-white"
                     href="https://github.com/cashupin"
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <i className="fab fa-github text-lg leading-lg text-white opacity-75" />
+                    <i className="fab fa-github text-3xl leading-lg text-white opacity-75" />
                   </a>
                 </li>
               </ul>
