da11c32afbb62995f23841a68f0762b064b55770
Juan Alvarez <jalvarezc96@gmail.com>
2022-08-31 22:48:01 -0400

    FIX: BG full height

--- src/components/BaseApp.tsx
+++ src/components/BaseApp.tsx
@@ -5,8 +5,8 @@ interface Props {
 }
 export const BaseApp: FC<Props> = ({ children }) => {
   return (
-    <section className="h-screen bg-gradient-to-b from-slate-800 via-slate-600 to-slate-800 ">
-      <div className="h-full bg-[url('../../public/img/bg.png')]">
+    <section className="h-full min-h-screen bg-gradient-to-b from-slate-800 via-slate-600 to-slate-800 ">
+      <div className="h-full min-h-screen bg-[url('../../public/img/bg.png')] bg-repeat">
         {children}
       </div>
     </section>
